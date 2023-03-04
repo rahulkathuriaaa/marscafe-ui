6ebc3b2382c1a3e9a6f17daf3ca18f9f41990971
Rahul Kathuria <rahulkathuria008@gmail.com>
2023-03-04 17:00:32 +0530

    first commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,8 +5,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#00040f",
-        secondary: "#00f6ff",
+        primary: "#311810",
+        secondary: "#B75740",
         dimWhite: "rgba(255, 255, 255, 0.7)",
         dimBlue: "rgba(9, 151, 124, 0.1)",
       },
